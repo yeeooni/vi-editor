5879f652bdbc5575138dd5a850638e1bbfac6f47
yeeooni <12.12kimiyeon@gmail.com>
2019-04-01 23:44:39 +0900

    첫 coding

--- HelloJava.java
+++ HelloJava.java
@@ -1,12 +1,7 @@
 public class HelloJava{
-		public static void main(String [] args){
+	public static void main(String [] args){
+	// 안녕, 자바야 잘부탁해.
+	System.out.println("Hello Java");
 
-
-				System.out.println("Hello Java");
-
-
-
-		}
-
-
+	}
 }
