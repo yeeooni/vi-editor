f65b8ff52db2e645b9c29cc7685b9f3bc2c8609a
yeeooni <12.12kimiyeon@gmail.com>
2019-04-01 23:21:40 +0900

    이것이 자바다 예제1)

--- PrinterExample.java
+++ PrinterExample.java
@@ -1,30 +1,26 @@
 public class PrinterExample{
 
-	//field 선언
-		
-		int num;
-		boolean x;
-		double dd;
-		String name;
+	//field
+	int num;
+	boolean x;
+	double dd;
+	String name;
 
-
-	//method 선언
-	public int println(int num){
-			
-			return num;
+	//method
+	public int println(int num){			
+		return num;
 	}
 
 	public boolean println(boolean x){
-			this.x = x;
-			return this.x;
+		this.x = x;
+		return this.x;
 	}
 
-	public double println(double dd){
-		
-			return dd;
+	public double println(double dd){		
+		return dd;
 	}
 
 	public String println(String name){
-			return name;		
+		return name;		
 	}
 }	
