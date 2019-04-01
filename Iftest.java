a22031a5d301c16319248b67507689db961fbff3
yeeooni <12.12kimiyeon@gmail.com>
2019-04-01 23:54:26 +0900

    예제6)

--- Iftest.java
+++ Iftest.java
@@ -1,6 +1,5 @@
  public class Iftest{
-		 public static void main(String [] args){
-
+	public static void main(String [] args){
 
 //사용자로부터 세 정수를 입력받아 그 중 가장 큰 수를 출력하는 프로그램을 작성
 //단, 삼항 연산자를 사용하지 말고 반드시 if문을 사용하여 작성
@@ -9,15 +8,18 @@
 	int num2 = 30;
 	int num3 = -10;
 
+		if(num1 > num2 && num1 > num3){
+			System.out.println("결과는 == " + num1);
+		} else if(num2 > num1 && num2 > num3){
+			System.out.println("결과는 == " + num2);
+		} else if(num3 > num1 && num3 > num2){
+			System.out.println("결과는 == " + num3);
+		}
+//삼항연산자
 
+	String result = (num1 > num2 ? num1 + "이 " + num2 + "보다 크다." : num1 + "이 " + num2 + "보다 작다."); 
+	System.out.println(result);
 
-			if(num1 > num2 && num1 > num3){
-					System.out.println(num1);
-			} else if(num2 > num1 && num2 > num3){
-				System.out.println(num2);
-			} else if(num3 > num1 && num3 > num2){
-					System.out.println(num3);
-			}
-		 }
+	}
  }
 
