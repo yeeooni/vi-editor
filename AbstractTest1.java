400f7e6cd861ac615996478be1a347cd4bfd9ea9
yeeooni <12.12kimiyeon@gmail.com>
2019-04-01 23:14:04 +0900

    kitri_Abstract

--- AbstractTest1.java
+++ AbstractTest1.java
@@ -3,17 +3,18 @@ import java.util.*;
 
 public class AbstractTest1{
 	public static void main(String [] args){
-				
-			Calendar cal1 = Calendar.getInstance();
+	
+	//Calendar 클래스의 객체를 생성하는데, 자기 자신의 클래스를 참조하여 상수값을 받아옴. getINstance();
+	Calendar cal1 = Calendar.getInstance();
 
-			int year = cal1.get(Calendar.YEAR);
-			int month = cal1.get(Calendar.MONTH) + 1;
-			int day = cal1.get(Calendar.DATE);
-			int hour = cal1.get(Calendar.HOUR_OF_DAY);
-			int minute = cal1.get(Calendar.MINUTE);
-			int second = cal1.get(Calendar.SECOND);
+	int year = cal1.get(Calendar.YEAR);
+	int month = cal1.get(Calendar.MONTH) + 1;
+	int day = cal1.get(Calendar.DATE);
+	int hour = cal1.get(Calendar.HOUR_OF_DAY);
+	int minute = cal1.get(Calendar.MINUTE);
+	int second = cal1.get(Calendar.SECOND);
 
-						System.out.println( year + " 년 " + month + " 월 " + day + " 일 " + hour + " 시 " + minute + " 분 " + second + " 초 ");
+	System.out.println( year + " 년 " + month + " 월 " + day + " 일 " + hour + " 시 " + minute + " 분 " + second + " 초 ");
 
 
 	}
